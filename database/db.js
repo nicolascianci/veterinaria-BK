f53e8d93d4b99def745e6057eebdd569eadcd10e
nicolascianci <martincianci@gmail.com>
2022-07-01 23:15:06 -0300

    comento console.log y instalcion de jade

--- database/db.js
+++ database/db.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 
 const connectDB = async() => {
   const url = process.env.DBLOCAL;
-console.log(url)
+// console.log(url)
   try {
     await mongoose.connect("mongodb://localhost:27017/Veterinaria")
     // await mongoose.connect("mongodb+srv://nicolascianci:<password>@bd.q6rtb.mongodb.net/test")
-    // console.log('DB conectada exitosamente')
+    console.log('DB conectada exitosamente')
   } catch (error) {
     console.log(error)
   }
