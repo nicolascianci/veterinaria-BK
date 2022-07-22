43d44906f6fff19f268562225010def3a185b8af
nicolascianci <martincianci@gmail.com>
2022-07-21 22:48:05 -0300

    se agrega validacion de token

---
+++ middleware/jwtValidator.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const jwt = require("jsonwebtoken")
+
+// const secretKey = process.env.SECRET_KEY
+
+const jwtValidator = async(req,res, next) => {
+    const {accessToken} = req.body
+    try {
+        const verify = jwt.verify(accessToken, 'veterinaria')        
+
+        if (verify) {
+            next()
+        }
+
+        return res.json({
+            message: 'Token invalido'
+        })
+} catch (error) {
+    return res.json({
+        message: 'No autorizado'
+    })
+}
+
+}
+
+
+module.exports = {jwtValidator}
