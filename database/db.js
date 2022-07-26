d28c2728c902da899932f208c2f8fb17d742d859
nicolascianci <martincianci@gmail.com>
2022-07-25 22:47:27 -0300

    se agrega la BD en la nube

--- database/db.js
+++ database/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 
 const connectDB = async() => {
   const url = process.env.DB;
- console.log(`${url}`)
+  console.log(`${url}`)
   try {
-    await mongoose.connect('mongodb://localhost:27017/Veterinaria')
-    // await mongoose.connect("mongodb+srv://nicolascianci:<password>@bd.q6rtb.mongodb.net/test")
+    //await mongoose.connect('mongodb://localhost:27017/Veterinaria')
+    await mongoose.connect("mongodb+srv://nico1996:4253709nico@veterinaria.w9ahm.mongodb.net/test")
     console.log('DB conectada exitosamente')
   } catch (error) {
     console.log(error)
